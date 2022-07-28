a3d9dd7ca0b61b37f23c9cc233b0379531fbd93d
mnee002 <106378614+mnee002@users.noreply.github.com>
2022-07-28 15:10:39 +0700

    Update pricingData.js

--- src/pages/pricingData.js
+++ src/pages/pricingData.js
@@ -14,28 +14,33 @@ export const priceTableColumns = [
 export const HDBPriceData =[
     {
         key:'1',
-        houseType:'1 Bedroom',
+        houseType:'1 Bed',
         price:'$158'
     },
     {
         key:'2',
-        houseType:'2 Bedroom',
+        houseType:'2 Bed',
         price:'$198'
     },
     {
         key:'3',
-        houseType:'3 Bedroom',
+        houseType:'3 Bed',
         price:'$248'
     },
     {
         key:'4',
-        houseType:'4 Bedroom',
+        houseType:'4 Bed',
         price:'$298'
     },
     {
         key:'5',
-        houseType:'5 Bedroom and above',
+        houseType:'5 Bed and above',
         price:'$348'
+    },
+    {
+        key:'6',
+        houseType:'EA/EM',
+        price:'$388'
     },
 ]
 
@@ -63,6 +68,6 @@ export const CondoPriceData =[
     {
         key:'5',
         houseType:'5 Bedroom and above',
-        price:'$368'
+        price:'$368 onwards'
     },
 ]
